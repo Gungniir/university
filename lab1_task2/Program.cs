c3e25e1d5f5063ca7ba78c83505244bf25a5712e
gungniir <vladimir.kurus@yandex.ru>
2021-09-02 22:14:33 +0700

    Lab1

---
+++ lab1_task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+// ReSharper disable SuggestVarOrType_BuiltInTypes
+
+namespace lab1_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны прямоугольника, а программа посчитает площадь и периметр :)");
+            
+            Console.Write("a: ");
+            string a = Console.ReadLine();
+            
+            long x;
+            
+            try
+            {
+                x = long.Parse(a!);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Не удалось распознать число \"{a}\"");
+                return;
+            }
+            
+            Console.Write("b: ");
+            string b = Console.ReadLine();
+
+            long y;
+            
+            try
+            {
+                y = long.Parse(b!);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Не удалось распознать число \"{b}\"");
+                return;
+            }
+
+            
+
+            Console.WriteLine($"Периметр: {x*2+y*2}");
+            Console.WriteLine($"Площадь: {x*y}");
+            
+        }
+    }
+}
---
+++ lab1_task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace lab1_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Депозитарный калькулятор");
+            Console.WriteLine("Введите необходимые параметры для рассчета ниже");
+
+            int deposit;
+            Console.Write("Сумма депозита ($): ");
+            try
+            {
+                deposit = int.Parse(Console.ReadLine()!);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удалось распознать число...");
+                return;
+            }
+
+            int rate;
+            Console.Write("Ставка (% годовых): ");
+            try
+            {
+                rate = int.Parse(Console.ReadLine()!);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удалось распознать число...");
+                return;
+            }
+
+            int duration;
+            Console.Write("Время (в месяцах): ");
+            try
+            {
+                duration = int.Parse(Console.ReadLine()!);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удалось распознать число...");
+                return;
+            }
+
+            int income = deposit * rate * duration / 12 / 100;
+            
+            Console.WriteLine($"Прибыль: {income}$, общая сумма выплаты: {deposit + income}$");
+        }
+    }
+}
